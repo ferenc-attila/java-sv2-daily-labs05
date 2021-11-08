787d2aa36586996a0c39d277a9b151c81222df27
Attila <attila.ferenc.80@gmail.com>
2021-11-08 16:41:39 +0100

    code cleaning

--- week05-labs-task/src/main/java/day01/MovieMain.java
+++ week05-labs-task/src/main/java/day01/MovieMain.java
@@ -9,18 +9,18 @@ public class MovieMain {
         Cinema cinema = new Cinema();
 
         cinema.addMovie(new Movie("Titanic", Arrays.asList(
-                LocalDateTime.of(2021,11,8,12,45),
-                LocalDateTime.of(2021,11,8,22,25)
+                LocalDateTime.of(2021, 11, 8, 12, 45),
+                LocalDateTime.of(2021, 11, 8, 22, 25)
         )));
         cinema.addMovie(new Movie("Jurassic Park", Arrays.asList(
-                LocalDateTime.of(2021,11,8,19,45),
-                LocalDateTime.of(2021,11,8,20,25),
-                LocalDateTime.of(2021,11,8,22,25)
+                LocalDateTime.of(2021, 11, 8, 19, 45),
+                LocalDateTime.of(2021, 11, 8, 20, 25),
+                LocalDateTime.of(2021, 11, 8, 22, 25)
         )));
 
-        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,10,45))); // []
-        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,19,45))); // [Jurassic Park]
-        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,22,25))); // [Titanic, Jurassic Park]
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021, 11, 8, 10, 45))); // []
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021, 11, 8, 19, 45))); // [Jurassic Park]
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021, 11, 8, 22, 25))); // [Titanic, Jurassic Park]
 
     }
 }
